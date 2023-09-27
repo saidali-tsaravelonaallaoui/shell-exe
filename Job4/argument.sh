da434fcee24f025740b53b7a097004eff10fe401
saidali-tsaravelonaallaoui <said-ali.tsaravelona-allaoui@laplateforme.io>
2023-09-27 10:56:56 +0200

    Update du Job4 - Modif du Script

--- Job4/argument.sh
+++ Job4/argument.sh
@@ -1,1 +1,4 @@
-cp $2 $1
+nouveau_fichier="$1"
+fichier_source="$2"
+cat "$fichier_source" > "$nouveau_fichier"
+echo "Le contenu de '$fichier_source' a été copié avec succès dans '$nouveau_fichier'."
