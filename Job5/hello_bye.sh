37f4ab7738ac1c2ac7f408cd6024cdab0da96277
saidali-tsaravelonaallaoui <said-ali.tsaravelona-allaoui@laplateforme.io>
2023-09-27 11:01:04 +0200

    Update du Job5 - Modif du Script

--- Job5/hello_bye.sh
+++ Job5/hello_bye.sh
@@ -1,7 +1,8 @@
-if [ $1 = "Hello" ]
-then
-    echo "Bonjour je suis un script !"
-elif [ $1 = "Bye" ]
-then
-    echo "Au revoir et bonne journée !"
+argument="$1"
+if [ "$argument" == "Hello" ]; then
+  echo "Bonjour, je suis un script !"
+elif [ "$argument" == "Bye" ]; then
+  echo "Au revoir et bonne journée !"
+else
+  echo "Argument non reconnu. Utilisez 'Hello' ou 'Bye'."
 fi
